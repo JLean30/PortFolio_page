6a5baef6a988d8e6d03ef220873b099cf838bac1
Jlean30 <Jlean30@protonmail.com>
2020-04-29 01:58:54 -0600

    first advances from final design

--- main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $('#init').click(function(){
-        console.log("click");
-        $('#container-profile').attr('hidden',false);
-        $('#init-section').attr('hidden',true);
-    });
-});
